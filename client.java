60a2c51a74c516508f24e4daa5b56ffe8b1721d9
JulienM14 <julienmedina314@gmail.com>
2023-11-17 16:39:22 -0500

    client changes

--- client.java
+++ client.java
@@ -5,12 +5,21 @@ class Client {
     public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("192.168.1.217", 7777);
+            Socket socket = new Socket(args[0], 7777);
             DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
-            System.out.print("Type a message > ");
-            String userInput = input.nextLine();
-            dout.writeUTF(userInput);
-            dout.flush();
+            DataInputStream dataIn = new DataInputStream(socket.getInputStream());
+            while (true) {
+                System.out.print("Type a message > ");
+                String userInput = input.nextLine();
+                dout.writeUTF(userInput);
+                String recieved = (String) dataIn.readUTF();
+                System.out.println(recieved);
+                dout.flush();
+                if (userInput.equalsIgnoreCase("exit")) {
+                    break;
+                }
+            }
+            dout.close();
             socket.close();
         }
         catch (Exception e) {
